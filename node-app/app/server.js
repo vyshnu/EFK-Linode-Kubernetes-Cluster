eb4af1230e2fd87eac2daa23b60bfd6580db8e4c
Vyshnavi Tekulapalli <21377454+vyshnu@users.noreply.github.com>
2021-08-09 01:49:18 +0530

    Add files via upload

---
+++ node-app/app/server.js
@@ -0,0 +1,21 @@
+const express = require('express'); //run thhe app
+const pino = require('pino'); //initiate logging
+
+const app = express();
+
+const logger = pino({
+    level: 'info',
+    timestamp: () => `,"time":"${new Date().toISOString()}"`
+});
+
+logger.info('hello elastic world');
+logger.info('This is some great stuff');
+logger.info('Some more entries for our logging');
+logger.info('another line');
+logger.info('This never stops');
+logger.info('Logging logging all the way');
+logger.info('I think this is enough');
+logger.info('nope, one more!');
+app.listen(3000, function () {
+    logger.info("app listening on port 3000!");
+});
